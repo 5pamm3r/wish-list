ed5b6df41d35eb75850f5d250bc3e2fa3d8be725
5pamm3r <cmassarino.dev@gmail.com>
2022-04-26 16:33:37 -0300

    styles

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Context } from '../Context'
+import './Form.css'
 
 function Form() {
     const [newValue, setNewValue] = React.useState('')
@@ -28,14 +29,15 @@ function Form() {
         <form onSubmit={onSubmit}> 
             {/*Un textarea a diferencia del input, permite que el texto largo se desplace hacia abajo. */}
             <textarea 
+                className='textarea'
                 autoFocus
                 value={newValue}
                 onChange={onChange}
                 placeholder='Ingrese un nuevo item'
             />
-            <div>
-                <button type='button' onClick={onCancel}>Cancelar</button>
-                <button type='submit'>Añadir</button> {/*Ejecuta el evento onSubmit */}
+            <div className='btn__container'>
+                <button className='btn__container-cancel' type='button' onClick={onCancel}>Cancelar</button>
+                <button className='btn__container-add' type='submit'>Añadir</button> {/*Ejecuta el evento onSubmit */}
             </div>
         </form>
     )
