2e5352db38ed35abbc4d132cff0631686b0ff202
5pamm3r <cmassarino.dev@gmail.com>
2022-08-08 18:00:52 -0300

    fix categories

--- src/components/Categorie/index.js
+++ src/components/Categorie/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Categorie.css";
 function Categorie(props) {
- 
+  const onClick = () => {
+    props.setOpenCategModal(prevState => !prevState)
+    console.log(props.category)
+  }
+
   return (
     <section className="categorie__container">
       <h2 className="categorie__title">CATEGORIES</h2>
       <ul className="categorieItem__container">
-        {props.CATEGORIES.map(props.render)}
-        {props.children}
+        {props.category.map(props.render)}
+        <div className="newCategory">
+          <div className="item__container">
+            <button className="btn__add" onClick={onClick}>+</button>
+            <span className="item__title">New Category</span>
+          </div>
+        </div>
       </ul>
     </section>
   );
