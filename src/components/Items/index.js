728262a69c08a36cb5aff297fa02137595c53771
5pamm3r <cmassarino.dev@gmail.com>
2022-08-10 20:43:09 -0300

    checkbox styles

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { DeleteButton } from "../DeleteButton";
 import "./Items.css";
 
 function Items(props) {
+  // const [checkColor, setCheckColor] = useState('')
+  const checkStyle = {
+    boxShadow: `0 0 2px #fff, 0 0 5px #fff, 0 0 10px ${props.categoryColor},
+    0 0 10px ${props.categoryColor}, 0 0 10px ${props.categoryColor}, 0 0 10px ${props.categoryColor}`,
+    border: `1px solid ${props.categoryColor}`,
+  }
+  
   return (
     <div
       className={`items__container `}
     >
-      <div
-        className={`item__check ${props.completed && "item__check-completed"} ${
-          props.categories === "personal" ? "item__check-personal" : "item__check-business"
-        }`}
-        onClick={props.onComplete}
-      >
+      <div id="checkColor"
+        className={`item__check ${!!props.completed && "item__check-completed"}`}
+        // onClick={props.onComplete}
+        onClick={()=>console.log(props.category)}
+        style={checkStyle}
+>
         <img
           src="https://img.icons8.com/external-tanah-basah-detailed-outline-tanah-basah/48/ffffff/external-check-user-interface-tanah-basah-detailed-outline-tanah-basah.png"
           alt="check icon"
