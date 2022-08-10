ddc6d8a9510b64eb211cb35e25d2fceacee26600
5pamm3r <cmassarino.dev@gmail.com>
2022-08-09 22:43:35 -0300

    color by category

--- src/components/Forms/formCategory.js
+++ src/components/Forms/formCategory.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FormCategory.css'
 
 function FormCategory({ category, addCategory }) {
   const [value, setValue] = React.useState('')
+  const [color, setColor] = useState('')
 
 //hacer que no se repita la misma categoria.
   const onSubmit = () => {
     if(!category.includes(value)){
-      addCategory(value)
+      addCategory(value, color)
     }
   }
 
-  const onChange = e => {
+  const onChangeValue = e => {
     setValue(e.target.value)
   }
-
+  const onChangeColor = (e) => {
+    setColor(e.target.value)
+  }
 
   return (
     <form className='FormCategory__container' onSubmit={onSubmit}>
-      <textarea placeholder='New category name' autoFocus onChange={onChange} />
+      <textarea placeholder='New category name' autoFocus onChange={onChangeValue} />
       <div className='categoryColor__container'>
         <span>Select a color</span>
-        <input type='color' />
+        <input type='color' id='categoryColor' onChange={onChangeColor} />
       </div>
       <button>Ir</button>
     </form>
