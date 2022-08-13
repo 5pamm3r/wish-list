e99437a5ed822f4a8ae8becc50efa43cc5cf0c11
5pamm3r <cmassarino.dev@gmail.com>
2022-08-13 15:34:31 -0300

    fix progress bar dom

--- src/components/CategorieItems/index.js
+++ src/components/CategorieItems/index.js
@@ -1,26 +1,19 @@
 import React from "react";
 import "./CategorieItems.css";
 function CategorieItems(props) {
-  
-  const [progress, setProgress] = React.useState('')
-
-  React.useEffect(()=>{
-    setProgress(props.progress)
-  },[props.progress, progress])
-
-  
-
   return (
     <div className={`item__container`}>
-      <span className="item__count">{props.count} tasks</span>
+      <span className="item__count">{props.totalItemCategory} tasks</span>
       <h2 className="item__title">{props.title}</h2>
       <div className="progress__container" id="progressContainer">
         <div
-          className={`progress__bar ${props.progressColor} `}
-          style={{ width: `${progress || 0}%` }}
-          ></div>
+          className={`progress__bar`}
+          style={{ width: `${props.progress || 0}%`, backgroundColor: `${props.color}` }}
+        ></div>
       </div>
-      <button className='delete__category' onClick={props.onDelete}>X</button>
+      <button className="delete__category" onClick={props.onDelete}>
+        X
+      </button>
     </div>
   );
 }
