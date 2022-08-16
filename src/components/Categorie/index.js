ca01c36e2510f503a41d028e270ddd54a3fcb17c
5pamm3r <cmassarino.dev@gmail.com>
2022-08-16 17:56:08 -0300

    change className

--- src/components/Categorie/index.js
+++ src/components/Categorie/index.js
@@ -10,8 +10,8 @@ function Categorie(props) {
       <h2 className="categorie__title">CATEGORIES</h2>
       <ul className="categorieItem__container">
         {props.category.map(props.render)}
-        <div className="newCategory">
-          <div className="item__container">
+        <div className="newCategory__container">
+          <div className="newCategory__item">
             <button className="btn__add" onClick={onClick}>+</button>
             <span className="item__title">New Category</span>
           </div>
