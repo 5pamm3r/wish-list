8c7d987beb795e42b92ede8287df55a985b11d25
5pamm3r <cmassarino.dev@gmail.com>
2022-08-17 15:40:41 -0300

    change item height

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./List.css";
 
 function List(props) {
-
+  useEffect(()=>{
+    const textarea = document.querySelectorAll('.item__textarea')
+    if(textarea.length>0){
+      const arr = [...textarea]
+      arr.map(e=>
+        e.style.height = `${e.scrollHeight}px`
+      )
+    }
+  },[props.item])
   
   return (
     <section className="list__container">
@@ -12,9 +20,6 @@ function List(props) {
         {props.loading && props.onLoading()}
         {!props.loading && !props.itemTotal && props.onEmpty()}
         {(!!props.itemTotal && !props.searchedTodos.length) && props.onEmptySearchResults()}
-
-        {/* {props.categItem.map(props.render)} */}
-
         {(!props.loading && !props.error) && props.searchedTodos.map(props.render)} 
         {props.children}
       </ul>
