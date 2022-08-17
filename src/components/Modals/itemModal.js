775155be19fce42b5f21dfae13a9d96808c0cd2a
5pamm3r <cmassarino.dev@gmail.com>
2022-08-17 15:42:38 -0300

    close modal with click

--- src/components/Modals/itemModal.js
+++ src/components/Modals/itemModal.js
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom';
 import './ItemModal.css'
 
-function ItemModal({ children }) {
+function ItemModal({ children, setOpenItemModal }) {
+  const onClick = (e) => {
+    if(e.target.id === 'itemModal'){
+      setOpenItemModal(prevState => !prevState)
+    }
+  }
     return ReactDOM.createPortal(
-        <div className='itemModal__background'>
+        <div id='itemModal' className='itemModal__background' onClick={onClick}>
             {children}
         </div>,
         document.getElementById('modal')
