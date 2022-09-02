c6bade18bde63d993e884bfb35ef0079d1fa5218
5pamm3r <cmassarino.dev@gmail.com>
2022-09-01 21:21:37 -0300

    Add react router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import NotFound from './components/Error/notFound';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./components/App";
+import NotFound from "./components/Error/notFound";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    // <App />
-    <NotFound />
-); 
-
+  <BrowserRouter>
+    <Routes>
+      <Route path="/wish-list" element={<App />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
