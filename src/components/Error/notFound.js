9fd8de75505a3844a0b9bfda10695278885b7fe6
5pamm3r <cmassarino.dev@gmail.com>
2022-09-01 21:22:57 -0300

    Error 404

--- src/components/Error/notFound.js
+++ src/components/Error/notFound.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './NotFound.css'
 import sateliteImg from '../../Assets/Images/satelite.png'
 
 function NotFound() {
-  return (
+    return (
     <div className='NotFound__container'>
       <h1 className='NotFound__title'><span className='NotFound__404'>404</span> Whoops!</h1>
       <p className='NotFound__subtitle'>We can't seem to find the space you're looking for :(</p>
-      <button className='NotFound__button'>Back to Home</button>
+      <Link to="/">
+        <button className='NotFound__button'>Back to Home</button>
+      </Link>
       <img src={sateliteImg} alt='satelite' />
     </div>
   )
