2fd6a11088977bb612315667c2a20d582ef7bd69
5pamm3r <cmassarino.dev@gmail.com>
2022-10-26 15:40:39 -0300

    Add effect when removed

--- src/components/DeleteButton/index.js
+++ src/components/DeleteButton/index.js
@@ -2,10 +2,14 @@ import './DeleteButton.css'
 import filledTrash from '../../Assets/Icons/filled-trash.png'
 
 function DeleteButton(props) {
+  const onClick = () => {
+    props.onMoveContainer()
+    props.onDelete()
+  }
     return (
         <button 
             className="delete__button"
-            onClick={props.onDelete}
+            onClick={onClick}
         ><img src={filledTrash} alt='delete icon' /></button>
     )
 }
