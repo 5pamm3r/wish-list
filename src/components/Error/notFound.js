f68f1beaaacc27faf5c976c1a2dd15991768b133
5pamm3r <cmassarino.dev@gmail.com>
2022-10-26 15:42:23 -0300

    Fix link to

--- src/components/Error/notFound.js
+++ src/components/Error/notFound.js
@@ -8,7 +8,7 @@ function NotFound() {
     <div className='NotFound__container'>
       <h1 className='NotFound__title'><span className='NotFound__404'>404</span> Whoops!</h1>
       <p className='NotFound__subtitle'>We can't seem to find the space you're looking for :(</p>
-      <Link to="/">
+      <Link to="/wish-list">
         <button className='NotFound__button'>Back to Home</button>
       </Link>
       <img src={sateliteImg} alt='satelite' />
