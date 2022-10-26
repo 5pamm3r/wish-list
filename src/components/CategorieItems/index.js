a2ab8432b3a3a8c6330f0635d71f3acb7e34380d
5pamm3r <cmassarino.dev@gmail.com>
2022-10-26 15:33:46 -0300

    Add 3d-card effect

--- src/components/CategorieItems/index.js
+++ src/components/CategorieItems/index.js
@@ -3,37 +3,39 @@ import DeleteCategory from "../DeleteCategory";
 import "./CategorieItems.css";
 
 function CategorieItems(props) {
-  const [stateDelete, setStateDelete] = React.useState(false)
+  const [stateDelete, setStateDelete] = React.useState(false);
   const barStyle = {
     width: `${props.progress | 0}%`,
     backgroundColor: `${props.color}`,
     boxShadow: `0 0 2px #fff, 0 0 5px #fff, 0 0 10px ${props.color},
-    0 0 10px ${props.color}, 0 0 10px ${props.color}, 0 0 10px ${props.color}`
+    0 0 10px ${props.color}, 0 0 10px ${props.color}, 0 0 10px ${props.color}`,
+  };
+  const onClick = () => {
+    setStateDelete((prevState) => !prevState);
   }
-  
-  if(!stateDelete) {
 
-    return (
-      <div className="item__container">
-      <span className="item__count">{props.totalItemCategory} tasks</span>
-      <h2 className="item__title">{props.title}</h2>
-      <div className="progress__container" id="progressContainer">
-        <div
-          className={`progress__bar`}
-          style={barStyle}
-        ></div>
-      </div>
-      <button className="delete__category" onClick={()=>{setStateDelete(prevState=>!prevState)}}>
-        X
-      </button>
+  return (
+    <div className={`item__container ${!!stateDelete && 'active'}`} onClick={onClick}>
+      {!stateDelete ? (
+        <div className={`face front ${stateDelete}`}>
+          <span className="item__count">{props.totalItemCategory} tasks</span>
+          <h2 className="item__title">{props.title}</h2>
+          <div className="progress__container" id="progressContainer">
+            <div className={`progress__bar`} style={barStyle}></div>
+          </div>
+        </div>
+      ) : (
+        <div className="face back">
+          <DeleteCategory
+            setStateDelete={setStateDelete}
+            state={stateDelete}
+            deleteCategory={props.deleteCategory}
+            deleteAllItemCategory={props.deleteAllItemCategory}
+          />
+        </div>
+      )}
     </div>
   );
-} else {
-  return (
-    // props.onDelete(setState)
-    <DeleteCategory setStateDelete={setStateDelete} deleteCategory={props.deleteCategory} deleteAllItemCategory={props.deleteAllItemCategory}/>
-  )
-}
 }
 
 export { CategorieItems };
