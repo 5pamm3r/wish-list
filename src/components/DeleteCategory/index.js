763a784a9c137494d1da820bc69147fa6b51fd7e
5pamm3r <cmassarino.dev@gmail.com>
2022-10-26 15:41:53 -0300

    Delete cancel button

--- src/components/DeleteCategory/index.js
+++ src/components/DeleteCategory/index.js
@@ -1,28 +1,22 @@
 import React from "react";
 import './DeleteCategory.css'
 
-function DeleteCategory({ setStateDelete, deleteCategory, deleteAllItemCategory }) {
+function DeleteCategory({ state,setStateDelete, deleteCategory, deleteAllItemCategory }) {
   const onDelete = () => {
     deleteCategory()
     deleteAllItemCategory()
   };
-  const onCancel = () => {
-    setStateDelete(prevState=>!prevState)
-  };
   return (
-    <div className="item__container">
+    <div className="Delete__container">
       <p className="Delete__title">
         ¿Eliminar categoría? 
       </p>
       <p className="Delete__subtitle">
         Perderás las tareas asignadas.
       </p>
-      <div className="DeleteCategoryButtons__container">
+      <div className="DeleteCategoryButton__container">
         <button onClick={onDelete} className="DeleteCategory__button">
           Eliminar
-        </button>
-        <button onClick={onCancel} className="CancelCategory__button">
-          Cancelar
         </button>
       </div>
     </div>
