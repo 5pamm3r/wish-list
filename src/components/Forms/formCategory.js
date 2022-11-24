0d8e08bd1daf249ffa080db299cf36c8a5fd83eb
5pamm3r <cmassarino.dev@gmail.com>
2022-11-23 21:16:40 -0300

    Add default colors

--- src/components/Forms/formCategory.js
+++ src/components/Forms/formCategory.js
@@ -5,17 +5,16 @@ function FormCategory({ category, addCategory, setOpenCategModal }) {
   const [value, setValue] = useState("");
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
+  const [active, setActive] = useState(false)
+
   const onSubmit = (event) => {
     event.preventDefault();
-    if (
-      category.find((e) => e.title.toLowerCase() === value.toLowerCase()) ===
-      undefined
-    ) {
-      addCategory(value, color);
-      setOpenCategModal((prevState) => !prevState);
-    } else {
+    if(category.find((e) => e.title.toLowerCase() === value.toLowerCase()) !== undefined) {
       setError(true);
+      return;
     }
+    addCategory(value, color);
+    setOpenCategModal(prevState=>!prevState);
   };
   const onChangeValue = (e) => {
     setValue(e.target.value);
@@ -23,29 +22,50 @@ function FormCategory({ category, addCategory, setOpenCategModal }) {
   const onChangeColor = (e) => {
     setColor(e.target.value);
   };
+  const onClickColor = (e) => {
+    setColor(window.getComputedStyle(document.getElementById(e.target.id)).getPropertyValue('background-color'))
+    //arreglar.
+    document.getElementById(e.target.id).className += ' active'
+    }
   return (
-    <form className="FormCategory__container" onSubmit={onSubmit}>
-      <textarea
-        className="Category__textarea"
-        placeholder="New category name"
-        autoFocus
-        required
-        onChange={onChangeValue}
-      />
-      {!!error && (
-        <p className="ErrorCategory__exists">The category already exists</p>
-      )}
-      <div className="CategoryColor__container">
-        <span className="Subtitle__color">Select color</span>
-        <input
-          className="Select__color"
-          type="color"
-          id="categoryColor"
-          onChange={onChangeColor}
-        />
+    <div className="FormCategory__container">
+      <div className="FormCategory-title__container">
+        <span>Add Category</span>
       </div>
-      <button className="CreateCategory__button">Create</button>
-    </form>
+      <form className="Form__container" onSubmit={onSubmit}>
+        <div className="Form-name__container">
+          <span>Category name</span>
+          <input
+            type="text"
+            className="Category__textarea"
+            placeholder="Type here..."
+            autoFocus
+            required
+            onChange={onChangeValue}
+          />
+        </div>
+        {!!error && (
+          <p className="ErrorCategory__exists">This category already exists</p>
+        )}
+        <div className="CategoryColor__container">
+          <span>Select color</span>
+          <div className="Colors__container">
+            <div className='first-color check' id="firstColor" onClick={onClickColor}></div>
+            <div className='second-color check' id='secondColor' onClick={onClickColor}></div>
+            <div className="third-color check" id="thirdColor" onClick={onClickColor}></div>
+            <div className="fourth-color check" id="fourthColor" onClick={onClickColor}></div>
+            <div className="fifth-color check" id="fifthColor" onClick={onClickColor}></div>
+            <input
+              className="Select__color"
+              type="color"
+              id="categoryColor"              
+              onChange={onChangeColor}
+            />
+          </div>
+        </div>
+        <button className="CreateCategory__button">Create</button>
+      </form>
+    </div>
   );
 }
 
