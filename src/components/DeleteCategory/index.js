66680916bf3eba5e44b9b0921c12ed842ef6a79c
5pamm3r <cmassarino.dev@gmail.com>
2023-02-24 09:47:38 -0300

    Change language

--- src/components/DeleteCategory/index.js
+++ src/components/DeleteCategory/index.js
@@ -1,22 +1,23 @@
 import React from "react";
-import './DeleteCategory.css'
+import "./DeleteCategory.css";
 
-function DeleteCategory({ state,setStateDelete, deleteCategory, deleteAllItemCategory }) {
+function DeleteCategory({
+  state,
+  setStateDelete,
+  deleteCategory,
+  deleteAllItemCategory,
+}) {
   const onDelete = () => {
-    deleteCategory()
-    deleteAllItemCategory()
+    deleteCategory();
+    deleteAllItemCategory();
   };
   return (
     <div className="Delete__container">
-      <p className="Delete__title">
-        ¿Eliminar categoría? 
-      </p>
-      <p className="Delete__subtitle">
-        Perderás las tareas asignadas.
-      </p>
+      <p className="Delete__title">Delete category?</p>
+      <p className="Delete__subtitle">Including all tasks.</p>
       <div className="DeleteCategoryButton__container">
         <button onClick={onDelete} className="DeleteCategory__button">
-          Eliminar
+          Delete
         </button>
       </div>
     </div>
